8e9f48be25954d292e2af32f090472209e2ac7db
firefly <chairwa@gmail.com>
2022-05-28 18:39:12 +0800

    tflite

--- wasmedge/extension_tensorflow.go
+++ wasmedge/extension_tensorflow.go
@@ -3,20 +3,11 @@
 package wasmedge
 
 /*
-#cgo linux LDFLAGS: -lwasmedge-tensorflow_c -lwasmedge-tensorflowlite_c -ltensorflow -ltensorflow_framework -ltensorflowlite_c
+#cgo linux LDFLAGS: -lwasmedge-tensorflowlite_c -ltensorflow -ltensorflow_framework -ltensorflowlite_c
 
-#include <wasmedge/wasmedge-tensorflow.h>
 #include <wasmedge/wasmedge-tensorflowlite.h>
 */
 import "C"
-
-func NewTensorflowImportObject() *ImportObject {
-	obj := C.WasmEdge_Tensorflow_ImportObjectCreate()
-	if obj == nil {
-		return nil
-	}
-	return &ImportObject{_inner: obj, _own: true}
-}
 
 func NewTensorflowLiteImportObject() *ImportObject {
 	obj := C.WasmEdge_TensorflowLite_ImportObjectCreate()
